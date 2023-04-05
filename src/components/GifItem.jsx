8107aa74a884c4744b1e61f6311b5d64d9f1f9b3
Alejandro Thon <alejandro96121@hotmail.com>
2023-04-05 12:08:15 -0400

    README.MD agregado

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 
-export const GifItem = ({ id, url,title }) => {
+export const GifItem = ({ id, url, title }) => {
 
     return (
         <div className='card'>
-        <img src={url} alt={title} />
-            
+            <img src={url} alt={title} />
+
             <h2>{title}</h2>
         </div>
     )
-   
+
 }
